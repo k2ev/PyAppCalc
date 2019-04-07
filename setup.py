e846450ddf3a30538d86d6cc0a6af254bb113ec4
k2ev <30880794+k2ev@users.noreply.github.com>
2019-04-07 00:20:25 -0400

    minor changes and getting ready to distribute

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
-from distutils.core import setup
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+Current = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = Current.joinpath("README.md").read_text()
 
 setup(
-    name='PyAppCalc',
+    name='PyAppCalc-2Simple',
     version='0.1',
     packages=['gui', 'Calcapp', 'CalcModel'],
     url='',
