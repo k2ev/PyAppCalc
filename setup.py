a1ec5accff25adb26bc97f0aa7f537d9a9f6f624
k2ev <30880794+k2ev@users.noreply.github.com>
2019-04-06 23:48:18 -0400

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyAppCalc',
+    version='0.1',
+    packages=['gui', 'Calcapp', 'CalcModel'],
+    url='',
+    license='MIT',
+    author='kiki',
+    author_email='kunal.shah1@gmail.com',
+    description=''
+)
